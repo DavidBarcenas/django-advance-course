d207d587d6d9d3156ad8c7acde07842652be1afa
David Barcenas <daveepro@outlook.com>
2021-07-17 18:24:27 -0500

    configure email sending

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import authenticate, password_validation
+from django.core.validators import RegexValidator
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.validators import UniqueValidator
-from django.core.validators import RegexValidator
 
 from cride.users.models import User, Profile
 
@@ -113,7 +115,29 @@ class UserSignUpSerializer(serializers.Serializer):
 
         user = User.objects.create_user(**data, is_verified=False)
         Profile.objects.create(user=user)
+        self.send_confirmation_email(user)
 
         return user
+
+    def send_confirmation_email(self, user):
+        """Send account verification link to given user."""
+
+        verification_token = self.gen_verification_token(user)
+
+        subject = f'Welcome @{user.username}: Verify your account to start using Comparte Ride.'
+        from_email = 'Comparte Ride <noreply@comparteride.com>'
+        content = render_to_string(
+            'emails/users/accunt_verification.html', {
+                'token': verification_token,
+                'user': user
+            }
+        )
+        msg = EmailMultiAlternatives(subject, content, from_email, [user.email])
+        msg.attach_alternative(content, 'text/html')
+        msg.send()
+        
+    def gen_verification_token(self, user):
+        """Create JWT token that the user can use to verify its account."""
+        return 'abc'
 
          
