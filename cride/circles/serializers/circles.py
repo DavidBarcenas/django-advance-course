60d7d537d5e4995bfad9ca16b4fff750c79663ce
David Barcenas <daveepro@outlook.com>
2021-07-19 21:42:13 -0500

    create circles and validate if limited

--- cride/circles/serializers/circles.py
+++ cride/circles/serializers/circles.py
@@ -1,3 +1,4 @@
+from django.db.models.fields import IntegerField
 from rest_framework import serializers
 
 from cride.circles.models import Circle
@@ -5,9 +6,18 @@ from cride.circles.models import Circle
 
 class CircleModelSerializer(serializers.ModelSerializer):
 
+    members_limit = serializers.IntegerField(
+        required = False,
+        min_value = 10,
+        max_value = 32000
+    )
+
+    is_limited = serializers.BooleanField(default=False)
+
     class Meta:
         
         model = Circle
+
         fields = (
         'id', 'name', 'slug_name',
         'about', 'picture',
@@ -15,3 +25,21 @@ class CircleModelSerializer(serializers.ModelSerializer):
         'is_verified', 'is_public',
         'is_limited', 'members_limit' 
         )
+
+        read_only_fields = (
+            'is_public',
+            'is_verified',
+            'rides_offered', 
+            'rides_taken',
+        )
+
+    def validate(self, data):
+        """Ensureboth members_limit and is_liited are present."""
+
+        members_limit = data.get('members_limit', None)
+        is_limited = data.get('is_limited', None)
+
+        if is_limited ^ bool(members_limit):
+            raise serializers.ValidationError('If circle is limited, a member limit must be provided.')
+
+        return data
--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,3 +1,4 @@
+from cride.circles.models.memberships import Membership
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -19,3 +20,18 @@ class CircleViewSet(viewsets.ModelViewSet):
             return queryset.filter(is_public=True)
 
         return queryset
+
+    def perform_create(self, serializer):
+        """Assign circle admin."""
+
+        user    = self.request.user
+        circle  = serializer.save()
+        profile = user.profile
+
+        Membership.objects.create(
+            user     = user,
+            profile  = profile,
+            circle   = circle,
+            is_admin = True,
+            remaining_invitations = 10
+        )
